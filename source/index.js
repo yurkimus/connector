198760e5834d4efac8a9c2ae7bb7cd42e2310441
Iurii <yurkimus@gmail.com>
2024-11-21 04:08:06 +0100

    Initial commit

---
+++ source/index.js
@@ -0,0 +1,89 @@
+export let Drivers = /** @type {const} */ ({
+  Minio: 'Minio',
+  Neo4j: 'Neo4j',
+  Postgres: 'Postgres',
+})
+
+let DriverConnectors = {
+  [Drivers.Minio]: instance => ({
+    connect: () =>
+      Promise
+        .resolve()
+        .then(partial(console.log, `[minio] connecting`))
+        .then(() => instance.listBuckets())
+        .then(partial(console.log, `[minio] connected\n`))
+        .catch(reason => {
+          console.log(`[minio] failed\n`, reason)
+          throw reason
+        }),
+
+    disconnect: () =>
+      Promise
+        .resolve()
+        .then(partial(console.log, `[minio] disconnecting`))
+        .then(partial(console.log, `[minio] disconnected`)),
+  }),
+
+  [Drivers.Neo4j]: instance => ({
+    connect: () =>
+      Promise
+        .resolve()
+        .then(partial(console.log, `[neo4j] connecting`))
+        .then(() => instance.getServerInfo())
+        .then(partial(console.log, `[neo4j] connected\n`))
+        .catch(reason => {
+          console.log(`[neo4j] failed\n`, reason)
+          throw reason
+        }),
+
+    disconnect: () =>
+      Promise
+        .resolve()
+        .then(partial(console.log, `[neo4j] disconnecting`))
+        .then(() => instance.close())
+        .then(partial(console.log, `[neo4j] disconnected`)),
+  }),
+
+  [Drivers.Postgres]: instance => ({
+    connect: () =>
+      Promise
+        .resolve()
+        .then(partial(console.log, `[postgres] connecting`))
+        .then(() => instance`select current_database()`)
+        .then(partial(console.log, `[postgres] connected\n`))
+        .catch(reason => {
+          console.log(`[postgres] failed\n`, reason)
+          throw reason
+        }),
+
+    disconnect: () =>
+      Promise
+        .resolve()
+        .then(partial(console.log, `[postgres] disconnecting`))
+        .then(() => instance.end())
+        .then(partial(console.log, `[postgres] disconnected`)),
+  }),
+}
+
+/**
+ * @param {keyof typeof Drivers} driver
+ * @param {*} instance
+ */
+export let connector = (driver, instance) => {
+  switch (driver) {
+    case Drivers.Minio:
+      return DriverConnectors[Drivers.Minio](instance)
+
+    case Drivers.Neo4j:
+      break
+
+    case Drivers.Postgres:
+      break
+
+    default:
+      throw new TypeError(
+        `'driver' must be one of: `
+          + `'${Object.values(Drivers).join(', ')}'.`,
+      )
+  }
+}
